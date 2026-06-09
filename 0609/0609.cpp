a84ecf06d32512c59ea6fcbead53ace7c2e0ef41
4D-12\koyo <a>
2026-06-09 14:44:16 +0900

    演習１

--- 0609/0609.cpp
+++ 0609/0609.cpp
@@ -1,6 +1,42 @@
 ﻿#include <iostream>
+#include <assert.h>
+
+class Weapon
+{
+public:
+	void Use() {}
+};
+
+class Player
+{
+	std::shared_ptr<Weapon> weapon;
+public:
+	Player(std::shared_ptr<Weapon> weapon1) : weapon(weapon1)
+	{
+	}
+	void Attack()
+	{
+		weapon->Use();
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	auto a = std::make_shared<Player>(std::make_shared<Weapon>());
+
+	std::cout << "a: " << a.use_count() << std::endl;
+
+	auto b = a;
+
+	std::cout << "a: " << a.use_count() << std::endl;
+	std::cout << "b: " << b.use_count() << std::endl;
+
+	auto c = std::move(a);
+
+	std::cout << "a: " << a.use_count() << std::endl;
+	std::cout << "b: " << b.use_count() << std::endl;
+	std::cout << "c: " << c.use_count() << std::endl;
+
+	assert(a != nullptr);
+	a->Attack();
 }
